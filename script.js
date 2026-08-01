34c124d5d1f73e88d3c40d83b8e1fd4153077418
bobyanmangaf-commits <bobyanmangaf@gmail.com>
2026-08-01 14:28:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const birthday = new Date("August 8, 2026 00:00:00").getTime();
+
+const timer = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = birthday - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+}, 1000);
